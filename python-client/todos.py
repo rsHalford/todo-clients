10868a563579a70132d823e18f68a3ae822be57b
Richard Halford <richardh.1995@googlemail.com>
2021-04-15 19:25:08 +0100

    🧱 basic implementations of crud operations. need to add CLI arguments and input verifiers

--- python-client/todos.py
+++ python-client/todos.py
@@ -1,11 +1,56 @@
-#!/usr/bin/python3
+#/usr/bin/python3
 
 import requests
 from api_access import USERNAME, PASSWORD
 
-get_about_resp = requests.get('https://todo.xhalford.com')
-get_todos_resp = requests.get('https://todo.xhalford.com/api/v1/todo', auth=(USERNAME, PASSWORD))
-get_about_resp_text = get_about_resp.text
-print(get_about_resp_text)
-get_todos_resp_json = get_todos_resp.json()
-print(get_todos_resp_json)
+url = 'https://todo.xhalford.com'
+auth = (USERNAME, PASSWORD)
+
+def get_about():
+    resp = requests.get(url)
+    text = resp.text
+    print(text)
+
+def create_todo():
+    print('Enter your todo\'s details.')
+    title = input('Title: ')
+    if len(title) > 10:
+        print('Error! Title must only be 10 characters long.')
+        return create_todo()
+    body = input('Body: ')
+    if len(body) > 100:
+        print('Error! Body must only be 100 characters long.')
+        return create_todo()
+    data = {'title': title, 'body': body}
+    resp = requests.post(url + '/api/v1/todo', auth=auth, json=data)
+    print(resp.json())
+
+def get_all_todos():
+    resp = requests.get(url + '/api/v1/todo', auth=auth)
+    data = resp.json()
+    print(data)
+
+def get_todo_by_id(id):
+    id = str(id)
+    resp = requests.get(url + '/api/v1/todo/' + id, auth=auth)
+    data = resp.json()
+    print(data)
+
+def update_todo_by_id():
+    id = input('Select which todo to update: ')
+    print('Enter your todo\'s details.')
+    title = input('Title: ')
+    if len(title) > 10:
+        print('Error! Title must only be 10 characters long.')
+        return create_todo()
+    body = input('Body: ')
+    if len(body) > 100:
+        print('Error! Body must only be 100 characters long.')
+        return create_todo()
+    data = {'title': title, 'body': body}
+    resp = requests.put(url + '/api/v1/todo/' + id, auth=auth, json=data)
+    print(resp.json())
+
+def delete_todo_by_id(id):
+    id = str(id)
+    resp = requests.delete(url + '/api/v1/todo/' + id, auth=auth)
